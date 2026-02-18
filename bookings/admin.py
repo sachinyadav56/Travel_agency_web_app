04c80f9d2560313f429bb310c879a855a1e289f8
sachinyadav56 <haramanyadav357@gmail.com>
2026-02-18 15:58:21 +0530

    Reduce font sizes in Travel Agency Dashboard by 40% and update admin styles

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,22 +1,3 @@
-# from django.contrib import admin
-# from django.db.models import Sum, F, ExpressionWrapper, DecimalField
-# from .models import Booking
-
-# from travel_project.admin import custom_admin_site
-
-# @admin.register(Booking, site=custom_admin_site)
-# class BookingAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'tour', 'seats_booked', 'total_price', 'paid', 'booking_date')
-#     list_filter = ('booking_date', 'tour', 'paid')
-#     search_fields = ('user__username', 'tour__title')
-#     date_hierarchy = 'booking_date'
-#     ordering = ('-booking_date',)
-#     readonly_fields = ('booking_date',)
-    
-#     def total_price(self, obj):
-#         return obj.seats_booked * obj.tour.price
-#     total_price.short_description = "Total Price (₹)"
-
 from django.contrib import admin
 from travel_project.admin import custom_admin_site
 from .models import Booking
